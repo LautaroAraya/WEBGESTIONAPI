7dd3eee821a693cefa9d16f627c01bc51d1379c9
Lautaro <lautyaraya65@gmail.com>
2023-06-02 15:08:45 -0300

    modi

--- js/app.js
+++ js/app.js
@@ -1,28 +1,34 @@
 const movies = {
-    render: () => {
+
+    obtenerTodos: () => {
+      //creamos una cosntante que tiene la URL de nuestra Api
       const urlAPI = 'https://pracprof2023-af4f.restdb.io/rest/peliculas?apikey=6467b09a0b60fc42f4e197fa';
-      const container = document.querySelector('#contenedorPeliculas');
-      let contentHTML = '';
-  
+      //creamos una constante que tendra una referencia directa con el DIV contenedor peliculas y nos permitira cargar de contenidos 
+      const divContenedorPeliculas = document.querySelector('#contenedorPeliculas');
+      //creamos una variable vacia que contendra todo el codigo html que vamos a insertar 
+      let contenidoHTML = '';
+      //obtenemos las peliculas en formato json 
       fetch(urlAPI)
         .then(res => res.json())
         .then((datos) => {
+          //enviamos a la consola de javascrip lo obetenido 
+          //console.log(datos[0])
+          //recorremos la coleccion de peliculas obtenienidas, obteniendo una referencia para cada una de las constantes "peli", por cada iteración 
           for (const peli of datos) {
-            let trailer_url = peli.trailer_url;
-            contentHTML += `
+            contenidoHTML += `
               <div class="col-md-4">
-                  <a href="${trailer_url}" target="_blank">
+                  <a href="${peli.trailer_url}" target="_blank">
                     <img src="${peli.portada_url}" alt="${peli.nombre}" class="img-thumbnail">
                   </a>
                   <h3 class="title">${peli.nombre}</h3>
               </div>`;
           }
-          container.innerHTML = contentHTML;
+          divContenedorPeliculas.innerHTML = contenidoHTML;
         })
     },
     agregarNuevaPelicula:() => {
       const urlAPI = 'https://pracprof2023-af4f.restdb.io/rest/peliculas?apikey=6467b09a0b60fc42f4e197fa';
-      const nuevaPeli = {"nombre":"John Wick","genero":"Acción/Aventura/Suspenso","duracion":101,"trailer_url":"https://www.youtube.com/watch?v=C0BMx-qxsP4","sinopsis":"John Wick es una franquicia estadounidense de suspenso y acción neo-noir creada por Derek Kolstad y producidas por Lionsgate a través de Summit Entertainment. La franquicia esta centrada en John Wick, un ex asesino a sueldo que se ve obligado a regresar al inframundo criminal que había abandonado anteriormente","portada_url":"https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/peliculas/john-wick-pacto-de-sangre/120113874-1-esl-ES/John-Wick-Pacto-de-sangre.png"};
+      const nuevaPeli = {"nombre":"Guardianes de la Galaxia","genero":"Ciencia Ficción/Cine de SuperHéroes","duracion":122,"trailer_url":"hhttps://www.youtube.com/watch?v=qdIuXCfUKM8&pp=ygUgdHJhaWxlciBndWFyZGlhbmVzIGRlIGxhIGdhbGF4aWE%3D","sinopsis":"Un aventurero espacial se convierte en la presa de unos cazadores de tesoros después de robar el orbe de un villano traicionero. Cuando descubre su poder, debe hallar la forma de unir a unos rivales para salvar al universo.","portada_url":"https://es.web.img3.acsta.net/pictures/14/06/17/14/04/353959.jpg"};
       fetch(urlAPI, {
       method: 'POST',
       headers: {
@@ -31,8 +37,8 @@ const movies = {
       body: JSON.stringify(nuevaPeli)
       })  
       .then(response => {
-        return movies.render();
+        return movies.obtenerTodos();
       });
     }
   };
-  movies.render();
+  movies.obtenerTodos();
